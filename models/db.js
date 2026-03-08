89deedc095f0f58851919652e0312b49c7bd87be
tuandtvt88 <tuandtvt88@gmail.com>
2026-03-08 13:47:30 +0700

    fix db6

--- models/db.js
+++ models/db.js
@@ -2,17 +2,19 @@ const mysql = require('mysql2/promise');
 
 console.log("DB HOST:", process.env.DB_HOST);
 console.log("DB USER:", process.env.DB_USER);
-console.log("DB DATABASE:", process.env.DB_NAME);
+console.log("DB DATABASE:", process.env.DB_DATABASE);
 console.log("DB PORT:", process.env.DB_PORT);
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE,
+  // Trả về DATE/DATETIME dạng string để tránh lệch múi giờ khi convert sang JS Date
+  dateStrings: true,
   waitForConnections: true,
-  connectionLimit: 10
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = pool;
