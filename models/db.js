73edf437e792055ed278583764f3f2b2b264bb36
tuandtvt88 <tuandtvt88@gmail.com>
2026-03-08 13:58:02 +0700

    fix env2

--- models/db.js
+++ models/db.js
@@ -7,9 +7,10 @@ console.log("DB PORT:", process.env.DB_PORT);
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME || process.env.DB_DATABASE,
   // Trả về DATE/DATETIME dạng string để tránh lệch múi giờ khi convert sang JS Date
   dateStrings: true,
   waitForConnections: true,
