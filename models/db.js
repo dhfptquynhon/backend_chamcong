c2f6f63075782c54837fe589d001470bd76ff4d0
tuandtvt88 <tuandtvt88@gmail.com>
2026-03-08 12:49:29 +0700

    fix db

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 
 const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
@@ -7,12 +7,15 @@ const pool = mysql.createPool({
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT,
-  dateStrings: true,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  dateStrings: true
 });
 
 console.log("DB HOST:", process.env.MYSQLHOST);
+console.log("DB USER:", process.env.MYSQLUSER);
+console.log("DB DATABASE:", process.env.MYSQLDATABASE);
+console.log("DB PORT:", process.env.MYSQLPORT);
 
 module.exports = pool;
