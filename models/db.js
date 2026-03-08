70ff3d0cb4377f37575866b0c5621a632c4e9ca3
tuandtvt88 <tuandtvt88@gmail.com>
2026-03-08 12:43:34 +0700

    fix mysql connection for railway

--- models/db.js
+++ models/db.js
@@ -1,13 +1,18 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  dateStrings: true,
   waitForConnections: true,
-  connectionLimit: 10
+  connectionLimit: 10,
+  queueLimit: 0
 });
+
+console.log("DB HOST:", process.env.MYSQLHOST);
 
 module.exports = pool;
